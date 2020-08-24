f5da4a6292c3e0781423cb88778f644160ee4e70
Paul Karayan <paulkarayan@gmail.com>
2020-08-23 19:25:00 -0700

    update the site

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Basic API Examples with Vercel Functions
+# Working Examples with Vercel Functions
 
-Intro
+Check the examples out at:
+[functions.immdom.com]
 
-_REMEMBER: This ain't gonna work unless you push things up to Vercel!_
+Get started on your own by doing the following:
+
+to run the dainty webapp:
+`yarn dev`
+
+_REMEMBER: These functions ain't gonna work unless you push 'em up to Vercel!_
 
 ## Basics
 
@@ -22,10 +28,59 @@ See INSERT ME for more details.
 
 ### Using Params
 
-## Working with Flask
+## Working with Python
 
-## Language Specific Notes
+### BaseHTTPRequestHandler
 
-### Python
+### Flask
 
-### Javascript
+## Getting Beyond This
+
+### Caveats
+
+These examples represent my (@paulkarayan) limited exploration of Vercel functions.
+Please let me know what I might be missing or doing wrong.
+
+The documentation provided by Vercel is generally good, but lacks working examples.
+In particular, there is a lot of information that appears to be out of date from the Zeit or now.sh
+while the grand vision seems to have been reigned in considerably.
+
+Overall - these are still really useful and amazing tools that are well worth the effort to understand.
+
+_IMPORTANT_
+You should probably treat these functions as middleware, not as full-fledged services themselves.
+For example, if you want to use Flask to do some simple post/preprocessing - that's perfect.
+However, if you want to build anything more fully featured, you should do it in a proper API server
+setup with Flask or other tech. Bonus: you'll not have to worry about lack of observability!
+
+Also - Node.js is clearly the preferred choice here. I don't mind it but take a look at the
+inability to use files with the Python example.
+
+### What Doesn't Make Sense / Work
+
+- I can't seem to get files to work with Python. see [link to my file]
+
+link to any follow up discussions.
+
+best info i found:
+https://github.com/vercel/vercel/discussions/4904
+
+- can flask apps have multiple routes or entry points?
+
+link to any follow up discussions.
+
+i thought dns.py (show example) would go to /dns
+
+From:
+https://vercel.com/docs/runtimes#advanced-usage/advanced-python-usage
+from flask import Flask, Response
+app = Flask(**name**)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+also see: https://github.com/vercel/now-examples/issues/163
+doesnt work like:
+https://camillovisini.com/barebone-serverless-flask-rest-api-on-zeit-now/
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import utilStyles from "../styles/utils.module.css";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Basic API examples with Vercel Functions
+          Welcome to Working examples with Vercel Functions
         </h1>
 
         <div className={styles.grid}>
@@ -22,30 +23,17 @@ export default function Home() {
 
           <a href="/api/js/files" className={styles.card}>
             <h3>Javascript - demonstrate using file system &rarr;</h3>
-            <p>
-              You can read from files in the _files directory. I find this
-              useful for configuration and credentials.
-            </p>
+            <p>You can read from files in the _files directory.</p>
           </a>
 
           <a href="/api/js/param?basicThing=Prada" className={styles.card}>
             <h3>Javascript - parameters example &rarr;</h3>
-            <p>
-              Shows how you can pass parameters to an API endpoint. What fun!
-            </p>
+            <p>Pass parameters to an API endpoint. What fun!</p>
           </a>
 
           <a href="/api/python/basic" className={styles.card}>
             <h3>Python - Basic API Example &rarr;</h3>
             <p>Available at /api/python/basic</p>
-          </a>
-
-          <a href="/api/python/files" className={styles.card}>
-            <h3>Python - demonstrate using file system &rarr;</h3>
-            <p>
-              You can read from files in the _files directory. I find this
-              useful for configuration and credentials.
-            </p>
           </a>
 
           <a href="/api/python/param?basicThing=Prada" className={styles.card}>
@@ -68,12 +56,18 @@ export default function Home() {
               </a>
             </p>
           </a>
+        </div>
+        <section className={utilStyles.headingMd}>
+          <p>[More Information]</p>
+          <p>... that you can shake a stick at</p>
+        </section>
 
+        <div className={styles.grid}>
           <a
             href="https://vercel.com/docs/serverless-functions/introduction"
             className={styles.card}
           >
-            <h3>Vercel Functions - documentation &rarr;</h3>
+            <h3>Vercel Official Docs - general &rarr;</h3>
             <p>
               Supports Python, Node.js, Ruby, and Golang. There are even more
               community Runtimes, too!
@@ -84,8 +78,23 @@ export default function Home() {
             href="https://vercel.com/docs/runtimes#official-runtimes/python"
             className={styles.card}
           >
-            <h3>Vercel Functions - important Python details &rarr;</h3>
-            <p>Available at /api/js/basic</p>
+            <h3>Vercel Official Docs - important Python details &rarr;</h3>
+          </a>
+
+          <a
+            href="https://github.com/paulkarayan/vercel-functions-immdom/blob/master/README.md"
+            className={styles.card}
+          >
+            <h3>Unofficial Readme from this redo &rarr;</h3>
+            <p>More details, gotchas, and caveats.</p>
+          </a>
+
+          <a
+            href="https://github.com/paulkarayan/immdom-examples"
+            className={styles.card}
+          >
+            <h3>More Immdom Fun &rarr;</h3>
+            <p>General catalogue and such. The fun starts here!</p>
           </a>
         </div>
       </main>
