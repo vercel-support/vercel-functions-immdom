02c64ba03442df4934aecd74daa0e000ed0fbbc0
Paul Karayan <paulkarayan@gmail.com>
2020-08-24 19:36:36 -0700

    catchall this time

--- api/js/param.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  const { basicThing = "Gucci" } = req.query;
-  res
-    .status(200)
-    .send(
-      `Hello, I'm basic - like ${basicThing}! Change what's basic by adding ?basicThing=<yourBasicThing>`
-    );
-};
